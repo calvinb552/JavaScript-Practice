cfd80cfbd436698baba5886dcac99db1737c5261
calvinb552 <116461964+calvinb552@users.noreply.github.com>
2026-07-13 21:04:27 -0400

    Target

--- script.js
+++ script.js
@@ -24,3 +24,38 @@ let func = (num1, num2) =>{return num1 + num2;}
 alert(func(1,3));//4
 
 //the prompt says no console errors but I can't see the console for some reason so I will just hope there aren't any errors
+//end bare minimum section
+
+
+
+async function getUserPostPreviews(userId){
+    try{
+    //promise.all does both promises at once
+    const [usersJSON, postsJSON] = await Promise.all([
+        //fetch returns a promise, so convert that to a json
+        (await fetch('https://jsonplaceholder.typicode.com/users')).json(),
+        (await fetch('https://jsonplaceholder.typicode.com/posts')).json()
+    ])
+
+    //filter the posts to only show ones that have the id wanted
+    const userPosts = postsJSON.filter(post => post.userId === userId)
+
+    //map the posts so that it has a title and preview
+    const mappedPosts = userPosts.map(({title, body}) => ({
+        title,
+        preview: body.slice(0,50)
+    }));
+    //returns promise
+    return mappedPosts;
+    }
+    catch(error){
+        alert(error);
+    }
+}
+
+//convert promise to JSON then stringify the result
+getUserPostPreviews(1).then(result => {
+    alert(JSON.stringify(result));
+});
+
+//end Target
